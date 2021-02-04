8b993a4882a67e3fdec768ac68ddd36d9df5c6e2
Kannan Ramaswamy <kannan.ramaswamy01@sap.com>
2021-02-04 15:46:34 +0530

    End of chapter 9

--- home.js
+++ home.js
@@ -1,18 +1,5 @@
 
-const button = document.getElementById('see-review');
-
-button.addEventListener('click', function() {
-
-    const review = document.getElementById('review');
-
-    if (review.classList.contains('d-none')) {
-        review.classList.remove('d-none');
-        button.textContent = 'CLOSE REVIEW';
-    }
-    else {
-        review.classList.add('d-none');
-        button.textContent = 'SEE REVIEW';
-    }
-    
-
-});
+const containers = 
+    document.getElementsByClassName('container');
+containers[0].classList.add('d-none');
+console.log(containers);
